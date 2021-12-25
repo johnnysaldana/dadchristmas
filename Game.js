76eca85e096fd1cb7ff0f8db2a73ddb0059c3455
Johnny <johnny.saldana99@gmail.com>
2021-12-25 09:53:06 -0500

    Final

--- Game.js
+++ Game.js
@@ -5,24 +5,28 @@ class Game {
       this.gameStart = false;
       this.displacement = 2/100;
       this.background = new Image();
-      this.background.src = "/assets/stock.png";
+      this.background.src = "/assets/steakhouse.jpeg";
       this.snoop = new Image();
-      this.snoop.src = "/assets/snoop.gif";
+      this.snoop.src = "/assets/bluebird.png";
+      this.music = new Audio("./assets/gamemusic.mp3");
       this.opened = false;
-      this.video = document.getElementById("myVideo");
       
       
       document.addEventListener("keydown", this.keyDownHandler.bind(this));
       document.addEventListener("keyup", this.keyUpHandler.bind(this));
     };
 
-
+    handleGameStart() {
+      this.music.play();
+      this.music.loop = true;
+      this.music.volume = 0.2;
+    }
 
     keyDownHandler(e) {
       if (!this.gameStart) {
         this.gameStart = true;
-        this.video.muted = !this.video.muted;
-        this.video.play();
+        this.handleGameStart()
+
       }
       if (e.keyCode === "o" ) {
         this.opened = true;
@@ -37,13 +41,13 @@ class Game {
     }
     drawBackground(ctx) {
       ctx.save();
-      ctx.drawImage(this.background, 0, 0, 1000, 400);
+      ctx.drawImage(this.background, 0, 0, 1200, 600);
       ctx.restore();
     }
 
     drawSnoop(ctx) {
       ctx.save();
-      ctx.drawImage(this.snoop, 50, 150, 200, 300);
+      ctx.drawImage(this.snoop, 400, 200, 350, 300);
       ctx.restore();
     }
     draw() {
